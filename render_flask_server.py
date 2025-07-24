ab46c6a3f43e079c636c48c5756dbdb78a1acd08
Kazu0525 <129633452+Kazu0525@users.noreply.github.com>
2025-07-24 11:35:48 +0900

    Update render_flask_server.py

--- render_flask_server.py
+++ render_flask_server.py
@@ -39,6 +39,6 @@ if __name__ == "__main__":
 import os
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5000))  # ← ここがポイント！
     app.run(host='0.0.0.0', port=port, debug=True)
 
