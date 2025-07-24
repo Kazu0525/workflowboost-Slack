a135210f41ef6f3416b91ecf529016631cd34d77
Kazu0525 <129633452+Kazu0525@users.noreply.github.com>
2025-07-24 11:17:05 +0900

    Update render_flask_server.py

--- render_flask_server.py
+++ render_flask_server.py
@@ -35,3 +35,9 @@ def chat():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5080))
+    app.run(host='0.0.0.0', port=port, debug=True)
